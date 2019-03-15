1cd459742d34c4fc721de68d8a1de7d783f6712d
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-03-15 23:34:25 +0900

    todoの追加と削除機能の追加

--- src/App.js
+++ src/App.js
@@ -9,17 +9,50 @@ class App extends Component {
         { id: 1, text: 'dataを表示する'},
         { id: 2, text: '簡単な構成を知る'},
       ],
+      newTodo: { test:''}
     };
+  };
+
+  handleInput = e =>{
+    const newId = Math.max.apply(null, this.state.todos.map(t => t.id)) +1;
+    this.setState({
+      newTodo:{id: newId, text: e.target.value }
+    });
+  };
+
+  createNewtoDoItem = () => {
+    this.setState(({ todos, newTodo }) =>({
+      todos: [
+        ...todos,
+        newTodo
+      ],
+    newTodo: { text:''}
+  }));
+};
+
+  deleteTodo = item => {
+    this.setState(({ todos}) => ({
+      todos: todos.filter((todo) => todo.id !== item.id)
+    }));
   };
 
   render() {
     return (
       <div className="App">
-      <ul>
-        {this.state.todos.map((item) =>{
-          return(<li>{item.text}</li>)
-        })}
-        </ul>
+        <ul>
+          {this.state.todos.map((item) =>{
+            return(
+              <li>
+              <span>{item.text}</span>
+              <span onClick={this.deleteTodo.bind(this, item)}>[x]</span>
+              </li>
+            )
+          })}
+          </ul>
+        <div>
+        <input type="text" value={this.state.newTodo.text} onChange={this.handleInput}/>
+        <button onClick={this.createNewtoDoItem}> + </button>
+        </div>
       </div>
     );
   }
